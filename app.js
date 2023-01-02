2d57d7c84f5385dbe56fd896ff23d3d97abf0d8c
brydenfb <brydenfb@gmail.com>
2023-01-02 20:48:02 +1300

    new themes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // Select The Elements
-var toggle_btn;
+var logo_btn;
 var big_wrapper;
 var hamburger_menu;
 
 function declare() {
   //toggle_btn = document.querySelector(".toggle-btn");
+  logo_btn = document.querySelector(".logo");
   big_wrapper = document.querySelector(".big-wrapper");
   hamburger_menu = document.querySelector(".hamburger-menu");
 }
@@ -15,17 +16,22 @@ declare();
 
 let dark = false;
 
+let currentClassIndex = 0;
+const cssClassList = ['light', 'dark', 'other'];
+
 function toggleAnimation() {
   // Clone the wrapper
-  dark = !dark;
+
   let clone = big_wrapper.cloneNode(true);
-  if (dark) {
-    clone.classList.remove("light");
-    clone.classList.add("dark");
-  } else {
-    clone.classList.remove("dark");
-    clone.classList.add("light");
-  }
+
+  // Increment the current class index, or reset it to 0 if it has reached the end of the list
+  
+  // Add the current class from the list to the clone element
+  clone.classList.remove(cssClassList[currentClassIndex]);
+
+  currentClassIndex = (currentClassIndex + 1) % cssClassList.length;
+
+  clone.classList.add(cssClassList[currentClassIndex]);
   clone.classList.add("copy");
   main.appendChild(clone);
 
@@ -36,12 +42,17 @@ function toggleAnimation() {
     big_wrapper.remove();
     clone.classList.remove("copy");
     // Reset Variables
+
     declare();
     events();
   });
 }
 
+
 function events() {
+  logo_btn.addEventListener("click", () => {
+    toggleAnimation();
+  });
   hamburger_menu.addEventListener("click", () => {
     big_wrapper.classList.toggle("active");
   });
